26e363dcd598033397a8220d9398404ad07c6348
Greg Coonrod <greg.coonrod@gmail.com>
2023-03-02 11:50:23 -0600

    Started multiple board layouts

--- asm/HelloWorld.65c02.asm
+++ asm/HelloWorld.65c02.asm
@@ -1,22 +1,97 @@
 .target "65c02"
 .format "bin"
 
-VIA  .equ $6000
-DDRB .equ $6002
+; 65C22 Addresses
+PORTB   .equ $6000
+PORTA   .equ $6001
+DDRB    .equ $6002
+DDRA    .equ $6003
 
-.org $fffc
-.word Initialize
+; HD44780U Control Signal masks
+DISP_E  .equ %10000000
+DISP_RW .equ %01000000
+DISP_RS .equ %00100000
+
+.org $fffc      ; Initialize Reset Vector
+.word start
 .word $0000
 
 .org $8000
+start:
+    ldx #$ff    ; Initialize stack to 01ff
+    txs
 
-Initialize  lda #$ff
-            sta DDRB   ;DDRB to output
+w65c22_init:
+    lda #$ff    ; Set all Port B pins to output 
+    sta DDRB
+    lda #$e0    ; Set 3 most significant bits of Port A to outputs
+    sta DDRA
 
-@Loop       lda #$55
-            sta VIA   ;on off pattern
+lcd_init:
+    lda #%00111000  ; Set 8 bit mode, 2 line display, 5x8 font
+    jsr lcd_instruction
+    lda #%00001110  ; Set display on, cursor on, blink off
+    jsr lcd_instruction
+    lda #%00000110  ; Increment and shift cursor, don't shift display
+    jsr lcd_instruction
 
-            lda #$aa
-            sta VIA   ;off on pattern
+print_hello:
+    lda #"H"    ; Send H
+    jsr lcd_print_char
+    lda #"e"
+    jsr lcd_print_char
+    lda #"l"
+    jsr lcd_print_char
+    lda #"l"
+    jsr lcd_print_char
+    lda #"o"
+    jsr lcd_print_char
+    lda #","
+    jsr lcd_print_char
+    lda #" "
+    jsr lcd_print_char
+    lda #"W"
+    jsr lcd_print_char
+    lda #"o"
+    jsr lcd_print_char
+    lda #"r"
+    jsr lcd_print_char
+    lda #"l"
+    jsr lcd_print_char
+    lda #"d"
+    jsr lcd_print_char
+    lda #"!"
+    jsr lcd_print_char
+    
+    
+wait_loop:
+    nop
+    jmp wait_loop
 
-            jmp @Loop
+lcd_instruction:
+    pha
+    sta PORTB
+
+    lda #0      ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #DISP_E ; Set display enable high
+    sta PORTA
+    lda #0      ; Set display enable low
+    sta PORTA
+    pla
+    rts
+
+lcd_print_char:
+    pha
+    sta PORTB
+
+    lda #DISP_RS      ; Set RS, Clear RW/E bits
+    sta PORTA
+
+    lda #(DISP_E | DISP_RS) ; Set display enable and register select high
+    sta PORTA
+    lda #DISP_RS      ; Set display enable low
+    sta PORTA
+    pla
+    rts
